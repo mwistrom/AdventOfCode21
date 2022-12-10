720bc7ac4ce48dc63a031a0a0b14021d61b74518
Mark Wistrom <mwistrom@gmail.com>
2022-12-09 19:22:10 -0800

    init

---
+++ aoc_01.py
@@ -0,0 +1,70 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_x(x):
+    for row in x:
+        for c in row:
+            print(c, end='')
+        print()
+    print()
+    print()
+
+def do_25():
+    x = []
+    with open('c:\\users\\noonan\\two_five.txt') as my_file:
+        for line in my_file:
+            a = list(line[:-1])
+            x.append(a)
+
+    h = len(x)
+    w = len(x[0])
+
+    print(h, "  ", w)
+
+    change = True
+    c = 0
+    while change:
+        print(c)
+        # print_x(x)
+        change = False
+        for i in range(h):
+            n = ['.'] * w
+            for j in range(w):
+                # print(i, " ", j)
+                if x[i][j % w] == 'v':
+                    n[j % w] = 'v'
+                    continue
+                if x[i][j % w] == '>' and x[i][(j+1) % w] == '.':
+                    n[(j+1) % w] = '>'
+                    change = True
+                    continue
+                if x[i][j % w] == '>':
+                    n[j % w] = '>'
+                    continue
+            x[i] = n
+
+        for j in range(w):
+            n = ['.'] * h
+            for i in range(h):
+                if x[i % h][j] == '>':
+                    n[i % h] = '>'
+                    continue
+                if x[i % h][j] == 'v' and x[(i+1) % h][j] == '.':
+                    n[(i+1) % h] = 'v'
+                    change = True
+                    continue
+                if x[i % h][j] == 'v':
+                    n[i % h] = 'v'
+                    continue
+            for i in range(h):
+                x[i][j] = n[i]
+        c = c + 1
+        print(c)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    do_25()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- main.py
+++ main.py
@@ -1,70 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def print_x(x):
-    for row in x:
-        for c in row:
-            print(c, end='')
-        print()
-    print()
-    print()
-
-def do_25():
-    x = []
-    with open('c:\\users\\noonan\\two_five.txt') as my_file:
+def do_01():
+    m = 0
+    a = 0
+    y = []
+    with open('C:\\Users\\noonan\\AppData\\Roaming\\JetBrains\\PyCharmCE2022.2\\scratches\\AOC_01.txt') as my_file:
         for line in my_file:
-            a = list(line[:-1])
-            x.append(a)
+            if len(line) == 1:
+                m = max(m, a)
+                y.append(a)
+                a=0
+                continue
 
-    h = len(x)
-    w = len(x[0])
+            x = int(line)
+            a = a + x
+    print(m)
+    y.sort(reverse=True)
+    print(y)
+    print(y[0] + y[1] + y[2])
 
-    print(h, "  ", w)
-
-    change = True
-    c = 0
-    while change:
-        print(c)
-        # print_x(x)
-        change = False
-        for i in range(h):
-            n = ['.'] * w
-            for j in range(w):
-                # print(i, " ", j)
-                if x[i][j % w] == 'v':
-                    n[j % w] = 'v'
-                    continue
-                if x[i][j % w] == '>' and x[i][(j+1) % w] == '.':
-                    n[(j+1) % w] = '>'
-                    change = True
-                    continue
-                if x[i][j % w] == '>':
-                    n[j % w] = '>'
-                    continue
-            x[i] = n
-
-        for j in range(w):
-            n = ['.'] * h
-            for i in range(h):
-                if x[i % h][j] == '>':
-                    n[i % h] = '>'
-                    continue
-                if x[i % h][j] == 'v' and x[(i+1) % h][j] == '.':
-                    n[(i+1) % h] = 'v'
-                    change = True
-                    continue
-                if x[i % h][j] == 'v':
-                    n[i % h] = 'v'
-                    continue
-            for i in range(h):
-                x[i][j] = n[i]
-        c = c + 1
-        print(c)
+def do_02a():
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    do_25()
+    do_02a()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
